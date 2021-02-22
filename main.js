e7e20f7994d4d50a4fc49c75f1fcd62bcc1b5e65
Amjad Allobadi <aallobadi@live.com>
2021-02-21 19:51:45 -0700

    Adding sleep and testing

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 var Car = require('./Components/Car.js');
+var sleep = require('sleep');
 
-var Car1 = new Car(1, 25, 55);
+var Car1 = new Car(1, 25, 'street1');
+var Car2 = new Car(1, 20, 'street1');
+var Car3 = new Car(1, 20, 'street2');
+var Car4 = new Car(4, 25, 'street2');
+var Car5 = new Car(5, 20, 'street2');
 
-console.log(Car1.increasseSpeed());
+let carsArray = [Car1, Car2, Car3, Car4, Car5];
 
-let speed = 10;
+carsArray.forEach((car) => {
+  console.log(car.increasseSpeed());
+  sleep.msleep(500);
+});
 
-for (var i = 0; i < speed; i++) {
-  console.log(Car1.increasseSpeed());
-}
+// console.log(Car1.increasseSpeed());
+
+// let speed = 10;
+
+// for (var i = 0; i < speed; i++) {
+//   sleep.msleep(500);
+//   console.log(Car1.increasseSpeed());
+// }
 
 console.log(Car1.toString());
